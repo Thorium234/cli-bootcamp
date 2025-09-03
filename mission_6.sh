f9f9dd707356b1f91484a6b0258fcc41ae2f42a4
Thorium234 <inyangalapaul5@gmail.com>
2025-09-03 08:59:55 +0300

    Added mission_6.sh and created report files

---
+++ mission_6.sh
@@ -0,0 +1,14 @@
+
+
+#!/bin/bash
+for i in {1..5}
+do
+  touch report$i.txt
+done
+# chmod +x mission_6.sh
+# ./mission_6.sh
+# git add mission_6.sh report1.txt report2.txt report3.txt report4.txt report5.txt
+# git commit -m "Added mission_6.sh and created report files"
+# git push origin main
+
+# End of script
